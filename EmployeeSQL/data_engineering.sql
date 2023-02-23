04bd556cf55b750c488514b7d3b448233584508f
mradovic10 <radovic.miodrag10@gmail.com>
2023-02-23 17:52:30 -0500

    README file updated, as well as minor details here and there. Challenge completed.

--- EmployeeSQL/data_engineering.sql
+++ EmployeeSQL/data_engineering.sql
@@ -1,25 +1,25 @@
 CREATE TABLE departments (
-	dept_no VARCHAR PRIMARY KEY,
-	dept_name VARCHAR
+	dept_no VARCHAR(4) PRIMARY KEY,
+	dept_name VARCHAR NOT NULL
 );
 
 SELECT * FROM departments;
 
 CREATE TABLE titles (
-	title_id VARCHAR PRIMARY KEY,
-	title VARCHAR
+	title_id VARCHAR(5) PRIMARY KEY,
+	title VARCHAR NOT NULL
 );
 
 SELECT * FROM titles;
 
 CREATE TABLE employees (
-	emp_no INTEGER PRIMARY KEY,
-	emp_title_id VARCHAR,
-	birth_date DATE,
-	first_name VARCHAR,
-	last_name VARCHAR,
+	emp_no SERIAL PRIMARY KEY,
+	emp_title_id VARCHAR(5) NOT NULL,
+	birth_date DATE NOT NULL,
+	first_name VARCHAR NOT NULL,
+	last_name VARCHAR NOT NULL,
 	sex VARCHAR,
-	hire_date DATE,
+	hire_date DATE NOT NULL,
 	FOREIGN KEY (emp_title_id) REFERENCES titles (title_id)
 );
 
@@ -27,7 +27,7 @@ SELECT * FROM employees;
 
 CREATE TABLE salaries (
 	emp_no INTEGER PRIMARY KEY,
-	salary INTEGER,
+	salary INTEGER NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
@@ -35,7 +35,7 @@ SELECT * FROM salaries;
 
 CREATE TABLE dept_emp (
 	emp_no INTEGER,
-	dept_no VARCHAR,
+	dept_no VARCHAR(4),
 	PRIMARY KEY (emp_no, dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
@@ -44,7 +44,7 @@ CREATE TABLE dept_emp (
 SELECT * FROM dept_emp;
 
 CREATE TABLE dept_manager (
-	dept_no VARCHAR,
+	dept_no VARCHAR(4),
 	emp_no INTEGER,
 	PRIMARY KEY (dept_no, emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 # sql-challenge
 Module 9 Challenge
 
-Entity Relationship Diagram: https://app.quickdatabasediagrams.com/#/d/rnOLX6
+This repository contains an EmployeeSQL folder that includes:
+
+- a PNG file of the Entity Relationship Diagram titled "data_modeling_ERD". Here is the link of the same file on QuickDBD: https://app.quickdatabasediagrams.com/#/d/rnOLX6;
+
+- a SQL file of table schemata titled "data_engineering";
+
+- a SQL file of necessary queries titled "data_analysis".
+
+
+The database contains six tables: "departments", "titles", "employees", "salaries", "dept_emp", and "dept_manager". Data was imported from CSV files into the tables using the pgAdmin 4 "Import/Export Data..." option.
+
+In the "data_analysis" file, eight various queries were created. Each one is preceded by its description.
+
+MR
